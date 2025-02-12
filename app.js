9f61c2db46b5bf66a5ae1ef4a414892f3d650a0f
Saifullah Siddiqui <saifullahshasiddiqui@gmail.com>
2025-02-12 22:03:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 
             emailjs.send("service_42gxs5i", "template_e0ck84k", params)
                 .then(function() {
-                    window.location.href = "https://www.instagram.com";
+                    window.location.href = "https://www.instagram.com/p/DD9G1FvJ7aL/?hl=en";
                 }, function(error) {
                     alert("Failed to send: " + JSON.stringify(error));
                 });
